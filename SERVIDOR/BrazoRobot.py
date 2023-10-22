5c9e5a121f1eb125a589902dbcb1d74458fc1533
Francisco Miranda <francisconehuenmiranda@gmail.com>
2023-10-22 20:17:41 -0300

    Clase BrazoRobot.py Add metodo ConectarRobot, desconectarRobot, EnviarComando

--- SERVIDOR/BrazoRobot.py
+++ SERVIDOR/BrazoRobot.py
@@ -1,3 +1,6 @@
+import serial
+import time
+
 from Punto import Punto
 class BrazoRobot():
     def __init__(self):
@@ -6,8 +9,11 @@ class BrazoRobot():
         # del efector final dado que no se inició la conexión con el robot.
         # Entonces se deja en parámetros inválidos
         self.velocidad = -10 # Lo mismo para la velocidad
+        #gregado de Fran -M-
+        self.conexion_establecida = False
+        self.puerto_serie = None
 
-    def conectarRobot(self):
+    def conectarRobot(self, puerto, baud_rate):
         # Me imagino que esta debe recibir los parámetros para la conexión
         # Aunque no se si se utiliza una velocidad por defecto o se puede indicar.
         # Revise el proyecto de Arduino y la velocidad de comunicación sí está dada por
@@ -15,4 +21,36 @@ class BrazoRobot():
         # Entonces solamente habría que habilitar la comunicación serial desde la computadora
         # hacia el arduino.
         
-        pass
+        try:
+            self.puerto_serie = serial.Serial(puerto, baud_rate)
+            self.conexion_establecida = True
+            print("Conexión exitosa con el puerto", puerto)
+
+        except serial.SerialException:
+            print("Error al intentar establecer la conexión.")
+            self.conexion_establecida = False
+            
+    def enviarComando(self, comando):
+        if self.conexion_establecida:
+            self.puerto_serie.write((comando + '\r'+'\n').encode('utf-8'))
+            # Ajusta un tiempo de espera (timeout) en segundos
+            self.puerto_serie.timeout = 1  # Puedes ajustar este valor según tus necesidades
+            data = b""
+            while True:
+                line = self.puerto_serie.readline()
+                if not line:
+                    break
+                data += line
+            return data.decode('utf-8').strip()
+        else:
+            return "No se pudo enviar el comando. La conexión no está establecida."
+
+
+    def desconectarRobot(self):
+        if self.conexion_establecida:
+            self.puerto_serie.close()
+            self.conexion_establecida = False
+            print("Conexión cerrada.")
+        else:
+            print("No hay una conexión activa que cerrar.")
+
