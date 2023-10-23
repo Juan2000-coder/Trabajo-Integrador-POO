a459e377bd56b16b0c05a1c21c21ea31b17828f4
Francisco Miranda <francisconehuenmiranda@gmail.com>
2023-10-23 11:02:35 -0300

    Clase BrazoRobot.py terminada y linea de test comentada al final que se debe borrar en un futuro, todos los metodos checkeados. Posible cambiios futuro en la implementacion del CLI. Los tkm

--- SERVIDOR/BrazoRobot.py
+++ SERVIDOR/BrazoRobot.py
@@ -25,12 +25,16 @@ class BrazoRobot():
             self.puerto_serie = serial.Serial(puerto, baud_rate)
             self.conexion_establecida = True
             print("Conexión exitosa con el puerto", puerto)
+            time.sleep(2) #Tiempo para que realice bien la conexion, si no envia los comandos muy rapido.
 
         except serial.SerialException:
             print("Error al intentar establecer la conexión.")
             self.conexion_establecida = False
             
     def enviarComando(self, comando):
+        #USO: respuesta = robot.enviarComando("G28")
+        #     print("Arduino dice:", respuesta)
+        #Primero almacenar la respuesta del comando, luego printear. 
         if self.conexion_establecida:
             self.puerto_serie.write((comando + '\r'+'\n').encode('utf-8'))
             # Ajusta un tiempo de espera (timeout) en segundos
@@ -53,4 +57,97 @@ class BrazoRobot():
             print("Conexión cerrada.")
         else:
             print("No hay una conexión activa que cerrar.")
+            
+    def seleccionarModo(self, modo):
+        if modo.lower() == "absolutas":
+            respuesta = self.enviarComando("G90")
+            print("Arduino dice:", respuesta)
+        elif modo.lower() == "relativas":
+            respuesta = self.enviarComando("G91")
+            print("Arduino dice:", respuesta)
+        else:
+            print("Modo de coordenadas no válido. Use 'absolutas' o 'relativas'.")
+            return  # No se envía un comando si el modo no es válido
+    
+    def movLineal(self, punto, velocidad=None):
+        if isinstance(punto, Punto):
+            if velocidad is not None:
+                comando = f"G1 X{punto.x:.2f} Y{punto.y:.2f} Z{punto.z:.2f} E{velocidad}"
+            else:
+                comando = f"G1 X{punto.x:.2f} Y{punto.y:.2f} Z{punto.z:.2f}"
+            
+            respuesta = self.enviarComando(comando)
+            print("Arduino dice:", respuesta)
+        else:
+            print("El argumento debe ser un objeto de la clase Punto.")
 
+    def activarPinza(self):
+            respuesta = self.enviarComando("M3")
+            print("Arduino dice:", respuesta)
+            
+    def desactivarPinza(self):
+            respuesta = self.enviarComando("M5")
+            print("Arduino dice:", respuesta)  
+
+    def home(self):
+            respuesta = self.enviarComando("G28")
+            print("Arduino dice:", respuesta)  
+    
+    def activarMotor(self):
+        print("Arduino dice: Motor activado")  
+            
+    def desactivarMotor(self):
+        print("Arduino dice: Motor desactivado")  
+
+'''
+Ejemplo test
+if __name__ == "__main__":
+robot = BrazoRobot()
+puerto = 'COM5'
+velocidad = 115200
+robot.conectarRobot(puerto, velocidad)
+
+#   while robot.conexion_establecida:
+#      comando = input("Ingresa un comando (o 'q' para salir): ")
+#     if comando.lower() == 'q':
+#        break
+    #   respuesta = robot.enviarComando(comando)
+    #  print("Arduino dice:", respuesta)
+respuesta = robot.enviarComando("G28")
+print("Arduino dice:", respuesta)
+
+
+robot.seleccionarModo("absolutas")
+
+
+# Opción 1: Modo coordenadas absolutas
+# Esto enviará "G90" a Arduino
+
+#robot.seleccionarModo("relativas")
+# Opción 2: Modo coordenadas relativas
+# Esto enviará "G91" a Arduino
+
+# Crear un objeto Punto con las coordenadas deseadas
+objetivo = Punto(10.0, 180.0, 130.0)
+robot.movLineal(objetivo)
+
+respuesta = robot.enviarComando("G28")
+print("Arduino dice:", respuesta)
+
+objetivo = Punto(10.0, 180.0, 130.0)
+robot.movLineal(objetivo, 5)
+
+robot.seleccionarModo("relativas")
+respuesta = robot.enviarComando("G0")
+print("Arduino dice:", respuesta)
+
+respuesta = robot.enviarComando("M5")
+print("Arduino dice:", respuesta)
+
+respuesta = robot.enviarComando("M3")
+print("Arduino dice:", respuesta)
+
+
+
+robot.desconectarRobot()
+'''
