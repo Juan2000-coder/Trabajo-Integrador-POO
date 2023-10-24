d9ef92e68e2f673de7975632744e3f0eb8461ff6
Juan <borquez.juan00@gmail.com>
2023-10-24 19:43:52 -0300

    Eliminación de las .dll organizacion de los directorios

--- etc/poo_eje2_xmlrpc/eje2_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from eje2_rpc_en_cli import XRConsola
-
-if __name__ == '__main__':
-    uncli = XRConsola()
-    uncli.prompt = ';-) '
-    uncli.cmdloop('Iniciando entrada de comandos...')
-
